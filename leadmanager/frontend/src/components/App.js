13ae32413c775cd32c853e746957304eb2881d8d
Shivam Sagar <shivamsagar888@gamil.com>
2019-09-24 11:09:34 +0530

    Till displaying and deletion

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './layouts/Header';
+import Dashboard from './leads/Dashboard';
+
+import store from '../store';
+import { Provider } from 'react-redux';
+
+class App extends Component {
+
+	render() {
+		return (
+			<Provider store={store}>
+				<Fragment>
+					<Header />
+					<div className="container">
+						<Dashboard />
+					</div>
+				</Fragment>
+			</Provider>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { getLeads, deleteLead } from '../../actions/leads';
+import PropTypes from 'prop-types';
+
+class Leads extends Component {
+
+	static propTypes = {
+		leads: PropTypes.array.isRequired
+	}
+
+	componentDidMount() {
+		this.props.getLeads();
+	}
+
+	render() {
+		console.log(this.props.leads);
+		return (
+			<Fragment>
+				<h2> Leads </h2>
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th> id </th>
+							<th> name </th>
+							<th> email  </th>
+							<th> message </th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{ this.props.leads.map(lead => (
+							<tr key={lead.id}>
+								<td> {lead.id} </td>
+								<td> {lead.name} </td>
+								<td> {lead.email} </td>
+								<td> {lead.message} </td>
+								<td> <button onClick={this.props.deleteLead.bind(this, lead.id)} className="btn btn-danger btn-sm"> DELETE </button> </td>
+							</tr>
+						)) }
+					</tbody>
+				</table>
+			</Fragment>
+		)
+	}
+}
+
+export default connect((state) => ({ leads: state.leads.leads }), { getLeads, deleteLead })(Leads)
